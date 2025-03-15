f1f27c98f27bdacf5b8e7d5914ee87561147ef00
aidana <ulanovaaidana2@gmail.com>
2025-03-15 12:07:12 +0600

    added desktop-2

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import  'react'
 import Block1 from '../../components/block-1/Block1'
+import Desktop from '../../components/other/Desktop'
 function Home() {
   return (
     <div>
     <Block1/>
+    <Desktop/>
       home
     </div>
   )
