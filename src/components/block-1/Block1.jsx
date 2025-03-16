f69804e2b60e251115d342a36cd8d122d3d573be
Nurislam <nurixkg5@gmail.com>
2025-03-16 16:58:51 +0800

    added new carousel

--- src/components/block-1/Block1.jsx
+++ src/components/block-1/Block1.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Carousel from 'react-bootstrap/Carousel';
-import banner from "../../assets/banner.png"
-import banner2 from "../../assets/banner2.png"
-import banner3 from "../../assets/banner3.png"
-import cakes from "../../assets/cakes.png"
+import banner1 from "../../assets/images/banner1.png"
+import banner2 from "../../assets/images/banner2.png"
+import banner3 from "../../assets/images/banner3.png"
+
+
 
 
 import "./block1.scss"
@@ -11,30 +12,17 @@ import "./block1.scss"
 function Block1() {
   return (
     <div className='carousel'>
-         <Carousel className='teg'>
-      <Carousel.Item>
-        {/* <ExampleCarouselImage text="First slide" /> */}
+         <Carousel >
+         <Carousel.Item>
+        <img src={banner3} alt="" />
+      </Carousel.Item>
+         <Carousel.Item>
         <img src={banner2} alt="" />
-        <Carousel.Caption className='carousel-content'>
-          <h1>Каждую пятницу<br /> 1+1</h1>
-          <p>В соответствии с принципом неопределенности, возмущение <br /> 
-          плотности синхронизует солитон. Расслоение синхронно.</p>
-          <img src={cakes} alt=""  className='cakes'/>
-          <img src={cakes} alt=""  className='cakes2'/>
-          
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        {/* <ExampleCarouselImage text="Third slide" /> */}
-        <img src={banner3} alt="" />
-        <Carousel.Caption className='carousel-content'>
-        <h1>Каждую пятницу<br /> 1+1</h1>
-          <p>В соответствии с принципом неопределенности, возмущение <br /> 
-          плотности синхронизует солитон. Расслоение синхронно.</p>
-          <img src={cakes} alt=""  className='cakes'/>
-          <img src={cakes} alt=""  className='cakes2'/>
-        </Carousel.Caption>
+        <img src={banner1} alt=""  width={1400}/>
       </Carousel.Item >
+
     </Carousel>
     </div>
   )
