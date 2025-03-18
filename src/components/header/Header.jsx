06c5e11b83300288a426896c4ea953fd270db49e
BAKYTq1 <bsainaev@gmail.com>
2025-03-18 23:28:41 +0600

    izmenenie

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
-function Header() {
-  return (
-    <div>
-      <nav>
-        <ul>
-      <Link to={"/WholesaleSales"}>
-        <li>Оптовые продажи</li>
-      </Link>
-
-        </ul>
-      </nav>
-      header
 import logo from "../../assets/svg/logo.svg"
 import { GiHamburgerMenu } from "react-icons/gi";
 import { FaRegUser } from "react-icons/fa";
@@ -26,12 +13,12 @@ function Header() {
           <img src={logo} alt="" />
         </div>
         <nav className='header-nav'>
-          <div className='catalog'>
+          <div className='catalogg'>
             <div className='icon'> <GiHamburgerMenu /></div>
              <h4>Каталог</h4>
           </div>
           <ul>
-            <li>Оптовые продажи</li>
+          <Link to={"/WholesaleSales"}> <li>Оптовые продажи</li> </Link>
             <li>Продажи в рознице</li>
             <li>Контакты</li>
             <li>Вакансии</li>
