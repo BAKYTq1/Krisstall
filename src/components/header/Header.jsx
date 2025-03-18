a993e2b6f7b59847424ce9dc89b559e0d74f8d56
nurhan004 <sharshenbekov777@gmail.com>
2025-03-18 14:50:08 +0600

    added WholesaleSales

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
     <div>
+      <nav>
+        <ul>
+      <Link to={"/WholesaleSales"}>
+        <li>Оптовые продажи</li>
+      </Link>
+
+        </ul>
+      </nav>
       header
     </div>
   )
