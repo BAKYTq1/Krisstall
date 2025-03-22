614ec8399bdea5481d33a08ac8b266addee02927
aidana <ulanovaaidana2@gmail.com>
2025-03-22 07:39:40 +0600

    auth-ui

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { myStore } from './redux/store.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 createRoot(document.getElementById('root')).render(
   <StrictMode >
     <Provider store={myStore}>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
-import  'react'
+import React from 'react'
 import Block1 from '../../components/block-1/Block1'
 import Desktop from '../../components/other/Desktop'
+import Auth from '../../components/auth/Auth'
 function Home() {
   return (
     <div>
     <Block1/>
     <Desktop/>
+    <Auth/>
       home
     </div>
   )
