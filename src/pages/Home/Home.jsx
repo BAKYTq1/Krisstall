4005569f7333c2fe2fd81bd4b3a9f327392bf335
aidana <ulanovaaidana2@gmail.com>
2025-03-22 08:03:05 +0600

    fix-mis

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import Block1 from '../../components/block-1/Block1'
 import Desktop from '../../components/other/Desktop'
 
 import Auth from '../../components/auth/Auth'
-=======
+
 import Block2 from '../../components/Block-2/Block2'
 import './Style.scss'
 
